f10119ca53c43b0b68d800ee340ad2691f3e1018
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-05-08 03:46:03 +0200

    add basic working udp low level driver

--- controller/src/main.c
+++ controller/src/main.c
@@ -1,6 +1,27 @@
+
 #include <stdio.h>
 
-int main() {
-	printf("Hello World\n");
-	return 0;
+#include "udp.h"
+#include "com.h"
+
+#define PORT 6969
+#define IP "127.0.0.1"
+
+int main(int argc, char **argv) {
+
+    (void) argc;
+    (void) argv;
+
+    udp_init();
+    udp_send(argv[1], 10);
+    char buf[20];
+    int len = udp_recv(buf, 20);
+    udp_deinit();
+    
+    for(int i=0; i<len; i++) {
+        printf("%c", buf[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
--- controller/src/udp.c
+++ controller/src/udp.c
@@ -10,87 +10,78 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <errno.h>
+
 
 #define IP              "127.0.0.1"
-#define PORT            6969
+#define CONTROL_PORT    6969
+#define BACKEND_PORT    6970
 #define SEND_TIMEOUT    100
 #define RECV_TIMEOUT    100
 
-static int socket = -1;
-static sockaddr_in backend_addr;
+static int sock_fd;
+static struct sockaddr_in backend_addr;
+static struct sockaddr_in controller_addr;
 
-int udp_init(int port, char *ip, int *sock) {
+int udp_init() {
 
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = 0;
-    hints.ai_protocol = 0;
-
-    struct addrinfo *serv_info;
-    int ret = getaddrinfo(NULL, port, &hints, &serv_info);
-    if (ret != 0) {
-        fprintf(stderr, "ERROR: udp init getaddrinfo: %s\n", gai_strerror(ret));
+    backend_addr.sin_family = AF_INET;
+    backend_addr.sin_port = htons(BACKEND_PORT);
+    if (inet_aton(IP, &backend_addr.sin_addr) == 0) {
+        fprintf(stderr, "\nERROR: udp init invalid backend_addr '%s'\n", IP);
         return -1;
     }
 
-    for(struct addrinfo *serv_info_itr = serv_info;
-        serv_info_itr != NULL;
-        serv_info_itr = serv_info_itr->ai_next) {
-
-        if ((*sock = socket(serv_info_itr->ai_family, serv_info_itr->ai_socktype,
-            serv_info_itr->ai_protocol)) < 0) {
-            fprintf(stderr, "udp init socket try next %s\n", strerror(errno));
-            continue;
-        }
-
-        if (bind(*sock, serv_info_itr->ai_addr, serv_info_itr->ai_addrlen) < 0) {
-            close(*sock);
-            fprintf(stderr, "udp init bind try next %s\n", strerror(errno));
-            continue;
-        }
-
-        break;
+    /* Create UDP socket */
+    sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock_fd < 0) {
+        fprintf(stderr, "\nERROR: udp init socket '%s'\n", strerror(errno));
+        return -2;
     }
 
-    if (serv_info_itr == NULL) {
-        fprintf(stderr, "ERROR: udp init failed install socket\n");
-        return -1;
+    if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0) {
+        fprintf(stderr, "\nERROR: udp init setsockopt failed '%s'\n", strerror(errno));
+        return -3;
     }
 
-    freeaddrinfo(serv_info);
+    controller_addr.sin_family = AF_INET;
+    controller_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    controller_addr.sin_port = htons(CONTROL_PORT);
+    if (bind(sock_fd, (struct sockaddr *)(&controller_addr),
+        sizeof(controller_addr)) < 0) {
+        fprintf(stderr, "\nERROR: udp bind socket '%s'\n", strerror(errno));
+        return -4;
+    }
 
-
-
-}
-
-int udp_deinit(int sock) {
-    close(sock);
     return 0;
 }
 
-int udp_send(int sock, char *data, int data_len) {
-    int len = sendto(sock, data, data_len, 0, p->ai_addr, p->ai_addrlen);
-    if (len  < 0) {
+int udp_deinit() {
+    close(sock_fd);
+    return 0;
+}
+
+int udp_send(char *data, int data_len) {
+    // TODO: add timeout
+    int len = sendto(sock_fd, data, data_len, 0,
+                     (struct sockaddr *)&backend_addr, sizeof(struct sockaddr_in));
+    if (len < 0) {
         fprintf(stderr, "ERROR: udp send %s\n", strerror(errno));
         return len;
     }
     return len;
 }
 
-int udp_recv(int sock, char **buf, int buf_size) {
+int udp_recv(char *buf, int buf_size) {
+    // TODO: add timeout
     // struct timeval read_timeout;
     // read_timeout.tv_sec = 0;
     // read_timeout.tv_usec = 10;
     // setsockopt(socketfd, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof read_timeout);
 
-    struct sockaddr_storage addr;       // not using the receivers information yet
-    socklen_t addr_len; = sizeof addr;  // ^
-
-    int len = recvfrom(sock, buf, MAXBUFLEN, 0, (struct sockaddr *)&addr, &addr_len));
+    int len = recvfrom(sock_fd, buf, buf_size, 0, NULL, NULL);
     if (len < 0) {
-        fprintf(stderr, "ERROR: udp recv %s\n", strerror(errno));
+        fprintf(stderr, "\nERROR: udp recv '%s'\n", strerror(errno));
         return len;
     }
     return len;
