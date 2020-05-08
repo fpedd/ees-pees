17bb6834705e1a8d64d73af8f36102af4a273c27
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-05-09 01:57:28 +0200

    fix broken delay

--- controller/src/util.c
+++ controller/src/util.c
@@ -16,18 +16,13 @@ int time_diff_stop(double *time) {
 }
 
 int delay(double sec) {
-    double start_time;
-    get_time(&start_time);
-    double current_time;
-    do {
-        get_time(&current_time);
-    } while (current_time < (start_time + sec));
+    double start_time = get_time();
+    while (get_time() < (start_time + sec));
     return 0;
 }
 
-int get_time(double *time) {
+double get_time() {
     struct timespec time_raw;
     clock_gettime(CLOCK_REALTIME, &time_raw);
-    *time = (double)time_raw.tv_sec + ((double)time_raw.tv_nsec / (double)1000000000);
-    return 0;
+    return (double)time_raw.tv_sec + ((double)time_raw.tv_nsec / (double)1000000000);
 }
