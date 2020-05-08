8ca67ec0ee520dddbcd5c92234bd958f8749c9ab
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-05-08 06:33:59 +0200

    python decoding with struct.iter_unpack

--- backend/com/com.py
+++ backend/com/com.py
@@ -1,15 +1,24 @@
+# https://www.journaldev.com/17401/python-struct-pack-unpack
+# https://docs.python.org/3/library/struct.html
+
 import socket
+import struct
+
 
 IP = "127.0.0.1"
 CONTROL_PORT = 6969
 BACKEND_PORT = 6970
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET,
+                     socket.SOCK_DGRAM)
 sock.bind((IP, BACKEND_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
-    data = data + "tsetset"
+    data, addr = sock.recvfrom(2048)
+    # print "received message:", data, len(data)
+    # data = data + "tsetset"
+    myit = struct.iter_unpack('f', data[8:])
+    # myit = struct.iter_unpack('ff', data)
+    for x in myit:
+        print(x)
     sock.sendto(data, (IP, CONTROL_PORT))
