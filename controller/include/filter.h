a729dc16ca28750f7325b7d42e9bfe359ed35824
pjoachims <39023593+pjoachims@users.noreply.github.com>
2020-05-09 16:24:24 +0200

    Revert "Controller"

--- controller/include/filter.h
+++
@@ -1,6 +0,0 @@
-#ifndef FILTER_H
-#define FILTER_H
-
-// This is where our filters for senors (and maybe sensor fusion) will go.
-
-#endif // FILTER_H
--- controller/include/safe.h
+++
@@ -1,7 +0,0 @@
-#ifndef SAFE_H
-#define SAFE_H
-
-// This will contain all code to check if the robot collides with some object.
-// This will then also provide funtions that help to keep the robot safe.
-
-#endif // SAFE_H
