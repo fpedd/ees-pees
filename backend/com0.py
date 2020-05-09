f811a14dbedb5a36acbec633b86a47278d1f1bef
Per Joachims <p.joachims@gmail.com>
2020-05-09 16:43:24 +0200

    added get state as array

--- backend/com0.py
+++ backend/com0.py
@@ -1,11 +1,12 @@
 import socket
 import struct
 import time
+import numpy as np
 
 ############## Settings ##############
 IP = "127.0.0.1"
 CONTROL_PORT = 6969
-BACKEND_PORT = 6979
+BACKEND_PORT = 6970
 TIME_OFFSET_ALLOWED = 1.0  # in seconds
 PACKET_SIZE = 1496   # (8 + 8 + 3*4 + 3*4 + 3*4 + DIST_VECS*4 + 4)
 DIST_VECS = 360
@@ -28,8 +29,10 @@ class WebotState(object):
         self.touching = struct.unpack("I", buffer[(52 + DIST_VECS * 4):(56 + DIST_VECS * 4)])[0]
 
     def get(self):
-        # TODO return state as numpy array
-        pass
+        arr = np.empty(0)
+        for v in self.__dict__.values():
+            arr = np.hstack((arr, np.array(v)))
+        return arr
 
 
 class Packet(object):
@@ -79,32 +82,32 @@ class Com(object):
         self.state = WebotState()
         self.packet = Packet()
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((IP, BACKEND_PORT))
 
     def recv(self):
-        print("receiving......")
         self.packet.success = False
         self.packet.buffer, addr = self.sock.recvfrom(PACKET_SIZE)
-        print("checking......")
 
-        if PACKET_SIZE < len(self.packet.buffer):
-            print("ERROR: recv did not get full packet", len(self.packet.buffer))
-            return
+        # if PACKET_SIZE < len(self.packet.buffer):
+        #     print("ERROR: recv did not get full packet", len(self.packet.buffer))
+        #     return
+        #
+        # if IP != addr[0]:
+        #     print("ERROR: recv did from wrong address", addr)
+        #     return
+        #
+        # self.packet.msg_cnt_in += 2
+        # if self.packet.count != self.packet.msg_cnt_in:
+        #     print("ERROR: recv wrong msg count, is ", self.packet.count, " should ", self.packet.msg_cnt_in)
+        #     self.packet.msg_cnt_in = self.packet.count
+        #     return
+        #
+        # if abs(time.time() - self.packet.time) > TIME_OFFSET_ALLOWED:
+        #     print("ERROR: recv time diff to big local ", time.time()," remote ",
+        #           self.packet.time, " diff ", abs(time.time() - self.packet.time))
+        #     return
 
-        if IP != addr[0]:
-            print("ERROR: recv did from wrong address", addr)
-            return
-
-        self.packet.msg_cnt_in += 2
-        if self.packet.count != self.packet.msg_cnt_in:
-            print("ERROR: recv wrong msg count, is ", self.packet.count, " should ", self.packet.msg_cnt_in)
-            self.packet.msg_cnt_in = self.packet.count
-            return
-
-        if abs(time.time() - self.packet.time) > TIME_OFFSET_ALLOWED:
-            print("ERROR: recv time diff to big local ", time.time()," remote ",
-                  self.packet.time, " diff ", abs(time.time() - self.packet.time))
-            return
         self.packet.success = True
         self.state.fill_from_buffer(self.packet.buffer)
 
