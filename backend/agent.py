166ddb94b31beb9bb7ef1eb4b0ed6dcfa7934e52
Per Joachims <p.joachims@gmail.com>
2020-05-10 17:25:44 +0200

    added first draft of agent to control robot with key arrows

--- backend/agent.py
+++ backend/agent.py
@@ -1,7 +1,10 @@
 import numpy as np
 import abc
+from pynput import keyboard
+import time
 
 import communicate
+import environment
 
 class Agent(abc.ABC):
     # @abc.abstractmethod
@@ -21,3 +24,41 @@ class RndWebotAgent(Agent):
         action.heading = np.random.randint(360)
         action.speed = np.random.random() * 200 - 100
         return action
+
+
+class WebotCtrAgent(Agent):
+    def __init__(self):
+        self.dheading = 3
+        self.dspeed = 3
+        self.env = environment.WebotsEnv()
+        self._init_action()
+
+    def _init_action(self):
+        self.act = communicate.WebotAction()
+        self.act.heading = np.random.randint(360)
+        self.act.speed = np.random.random() * 200 - 100
+
+    def action(self):
+        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
+            listener.join()
+
+    def on_press(self, key):
+        condition = False
+        if key == keyboard.Key.up:
+            self.act.speed += self.dspeed
+            condition = True
+        if key == keyboard.Key.down:
+            self.act.speed -= self.dspeed
+            condition = True
+        if key == keyboard.Key.left:
+            self.act.heading -= self.dheading
+            condition = True
+        if key == keyboard.Key.right:
+            self.act.heading += self.dheading
+            condition = True
+        if condition is True:
+            self.env.step(self.act)
+
+    def on_release(self, key):
+        if key == keyboard.Key.esc:
+            return False
