76d9698eef3ca99bce2718076095d49f4289d558
Per Joachims <p.joachims@gmail.com>
2020-05-13 11:02:10 +0200

    adding PacketError Enum

--- backend/communicate.py
+++ backend/communicate.py
@@ -3,10 +3,11 @@ import struct
 import time
 import numpy as np
 import configparser
+from enum import Enum
 
 
 class Config(configparser.ConfigParser):
-    def __init__(self, config_file="config_com.ini"):
+    def __init__(self, config_file="config.ini"):
         super(Config, self).__init__()
         self.read(config_file)
 
@@ -58,6 +59,13 @@ class Config(configparser.ConfigParser):
         else:
             return 360
 
+    @property
+    def MAX_DISTANCE(self):
+        if self.success is True:
+            return float(self.get('Webot', 'MAX_DISTANCE'))
+        else:
+            return 100
+
 
 class WebotState(object):
     def __init__(self):
@@ -73,12 +81,20 @@ class WebotState(object):
         self.compass = struct.unpack('3f', buffer[40:52])
         self.distance = struct.unpack("{}f".format(DIST_VECS), buffer[52:(52 + DIST_VECS * 4)])
         self.touching = struct.unpack("I", buffer[(52 + DIST_VECS * 4):(56 + DIST_VECS * 4)])[0]
+        self._to_array()
+
+    def _to_array(self):
+        for v in self.__dict__.values():
+            v = np.array(v)
 
     def get(self):
         arr = np.empty(0)
         for v in self.__dict__.values():
-            arr = np.hstack((arr, np.array(v)))
+            arr = np.hstack((arr, v))
         return arr
+
+    def describe(self):
+        strr = "[0, 1]: "
 
     @property
     def num_of_sensors(self):
@@ -99,12 +115,20 @@ class WebotState(object):
         return len(self.compass)
 
 
+class PacketError(Enum):
+    UNITILIZED = -1
+    NO_ERROR = 0
+    SIZE = 1
+    IP = 2
+    COUNT = 3
+    TIME = 4
+
 
 class Packet(object):
     def __init__(self):
         self.buffer = None
         self.time_in = None
-        self.success = False
+        self.error = PacketError.UNITILIZED
 
     @property
     def count(self):
@@ -113,6 +137,14 @@ class Packet(object):
     @property
     def time(self):
         return struct.unpack('d', self.buffer[8:16])[0]
+
+    @property
+    def success(self):
+        if self.error == PacketError.UNITILIZED:
+            return None
+        elif self.error == PacketError.NO_ERROR:
+            return True
+        return False
 
 
 class WebotAction(object):
@@ -172,8 +204,6 @@ class Com(object):
         self.history.append([self.packet.time, self.packet])
 
     def recv(self):
-        self.packet.success = False
-
         t1 = time.time()
         self.packet.buffer, addr = self.sock.recvfrom(self.conf.PACKET_SIZE)
         self.latency = t1 - time.time()
@@ -181,26 +211,30 @@ class Com(object):
         self._update_history()
         self.msg_cnt_in += 2
 
-        # if PACKET_SIZE < len(self.packet.buffer):
-        #     print("ERROR: recv did not get full packet", len(self.packet.buffer))
-        #     return
-        #
-        # if IP != addr[0]:
-        #     print("ERROR: recv did from wrong address", addr)
-        #     return
-        #
-        # if self.packet.count != self.packet.msg_cnt_in:
-        #     print("ERROR: recv wrong msg count, is ", self.packet.count, " should ", self.packet.msg_cnt_in)
-        #     self.packet.msg_cnt_in = self.packet.count
-        #     return
-        #
-        # if abs(time.time() - self.packet.time) > TIME_OFFSET_ALLOWED:
-        #     print("ERROR: recv time diff to big local ", time.time()," remote ",
-        #           self.packet.time, " diff ", abs(time.time() - self.packet.time))
+        if self.conf.PACKET_SIZE < len(self.packet.buffer):
+            self.packet.error = PacketError.SIZE
+            print("ERROR: recv did not get full packet", len(self.packet.buffer))
+            return
+
+        if self.conf.IP != addr[0]:
+            self.packet.error = PacketError.IP
+            print("ERROR: recv did from wrong address", addr)
+            return
+
+        # if self.packet.count != self.msg_cnt_in:
+        #     self.packet.error = PacketError.COUNT
+        #     print("ERROR: recv wrong msg count, is ", self.packet.count, " should ", self.msg_cnt_in)
+        #     self.msg_cnt_in = self.packet.count
         #     return
 
-        self.packet.success = True
+        if abs(time.time() - self.packet.time) > self.conf.TIME_OFFSET_ALLOWED:
+            self.packet.error = PacketError.TIME
+            print("ERROR: recv time diff to big local ", time.time()," remote ",
+                  self.packet.time, " diff ", abs(time.time() - self.packet.time))
+            return
+
         self.state.fill_from_buffer(self.packet.buffer, self.conf.DIST_VECS)
+        return self.state
 
     def send(self, action:WebotAction):
         data = struct.pack('Qdff', self.msg_cnt_out, time.time(),
