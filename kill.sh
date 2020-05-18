c5e510e230de35a192c4ea939741ef232c4e3f72
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-05-18 08:17:56 +0200

    single script to start/stop the stack :)

---
+++ kill.sh
@@ -0,0 +1,17 @@
+### This bash script should help you stop the communication stack.
+### In order to make this script executable on your system, run:
+###
+###   chmod +x kill.sh
+###
+
+#!/bin/sh
+# set -e
+
+echo "Killing webots"
+pkill webots
+
+echo "Killing james"
+pkill james
+
+echo "Killing controller"
+pkill controller
--- run.sh
+++ run.sh
@@ -1,18 +1,48 @@
-# This bash script should help you start the communication stack.
-# In order to make this script executable on your system, run:
-#
-#   chmod +x run.sh
-#
+### This bash script should help you start the communication stack.
+### In order to make this script executable on your system, run:
+###
+###   chmod +x run.sh
+###
 
+#!/bin/sh
+set -e
 
+### Path to External Controller
+EXT_CTRL=/controller
 
-WEBOTS_HOME=/usr/local/webots
-GIT_PATH=/home/fabian/uni/ees-pees
+### Path to your local Webots installation
+export WEBOTS_HOME=/usr/local/webots
 
+### Path to Webots World and Internal Controller
 WORLD_FILE=/webots/worlds/prototypes.wbt
+INT_CTRL=/webots/controllers/internal_ctrl
 
+### Path to Backend Script
+BCKND_SCRIPT=/backend/james.py
 
-CONTROLLER=/controller/
+### Terminal Emulator you want to use
+TERM_EM=gnome-terminal\ -x
+# TERM_EM=xterm\ -e
 
+### Uncomment this if you want to run the processes detached
+# DETACH=&
 
-webots ${GIT_PATH}${WORLD_FILE}
+### This should point to where this bash script is at
+# GIT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "Clean and compile the External Controller"
+make clean -C .${EXT_CTRL}
+make all -C .${EXT_CTRL}
+
+echo "Clean and compile the Interal Controller"
+make clean -C .${INT_CTRL}
+make all -C .${INT_CTRL}
+
+echo "Start the External Controller"
+${TERM_EM} .${EXT_CTRL}/build/controller ${DETACH}
+
+echo "Start Webots and the Internal Controller"
+${TERM_EM} webots .${WORLD_FILE} ${DETACH}
+
+echo "Start the Backend"
+${TERM_EM} python3 .${BCKND_SCRIPT} ${DETACH}
