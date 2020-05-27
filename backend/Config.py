87da5be6da21be789632a563f59046924fa3bebe
Per Joachims <p.joachims@gmail.com>
2020-05-27 19:12:19 +0200

    updated standard webot config data

--- backend/Config.py
+++ backend/Config.py
@@ -1,10 +1,15 @@
 class WebotConfig(object):
 
     def __init__(self):
+        # network
         self.IP = "127.0.0.1"
         self.CONTROL_PORT = 6969
         self.BACKEND_PORT = 6970
         self.PACKET_SIZE = 1496
-        self.DIST_VECS = 360
         self.TIME_OFFSET_ALLOWED = 1.0
-        self.length = 100
+
+        # environment, sensor and robot
+        self.DIST_VECS = 360
+        self.length = 10
+        self.lidar_min = 0.12
+        self.lidar_max = 3.5
