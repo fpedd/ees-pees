558ffab43d44bd4f508c0dc94fa22ca8624ff222
Per Joachims <p.joachims@gmail.com>
2020-05-27 14:07:58 +0200

    basic action mapping for Real Webot environment

--- backend/Action.py
+++ backend/Action.py
@@ -1,6 +1,9 @@
 import numpy as np
 from gym.spaces import Tuple, Box, Discrete
 
+import utils
+from webot import WebotState, WebotAction
+
 
 class Action(object):
     def __init__(self):
@@ -11,32 +14,76 @@ class Action(object):
 
 
 class DiscreteAction(Action):
-    def __init__(self, mode="flatten", directions=None, steps=None):
+    """
+    Steps, Directions must be of the form 2k + 1, k>=1
+    ----------------------DIRECTIONS--------------------
+    -    [0]                 [1]               [2]
+    - [0] heading -= 0.2     ....              ....
+    -     speed -= 0.2
+    S
+    P [1] left 36°        DO NOTHING           ....
+    E     speed += 0
+    E
+    D [2] left 36°           ....              +36°
+    S     speed += 0.2                     speed += 0.2
+    -
+    -
+
+    Tuple: (DIRECTIONS, STEPS)
+    Flat: DIRECTIONS * STEPS
+    """
+
+    def __init__(self, directions=3, speeds=3, dspeed=0.2, dhead=0.2,
+                 mode="flatten"):
         super(DiscreteAction, self).__init__()
         self.mode = mode
         self.directions = directions
-        self.steps = steps
-
-    def _init(self, env):
-        self.env = env
+        self.speeds = speeds
+        self.dhead = dhead
+        self.dspeed = dspeed
+        self.action_tuple = (directions, speeds)
         self._set_action_space()
+        self._set_mapping_space()
+
+    @property
+    def number_of_actions(self):
+        if self.mode == "flatten":
+            return self.action_tuple[0] * self.action_tuple[1] + 1
+        elif self.mode == "tuple":
+            return self.action_tuple[0] * (self.action_tuple[1] + 1)
 
     def _set_action_space(self):
         if self.mode == "flatten":
-            self.action_space = Discrete(self.env.act_tpl[0] *
-                                         self.env.act_tpl[1])
+            self.action_space = Discrete(self.action_tuple[0] *
+                                         self.action_tuple[1] + 1)
         elif self.mode == "tuple":
-            self.action_space = Tuple((Discrete(self.env.act_tpl[0]),
-                                       Discrete(self.env.act_tpl[1])))
+            self.action_space = Tuple((Discrete(self.action_tuple[0]),
+                                       Discrete(self.action_tuple[1] + 1)))
 
+    def _set_mapping_space(self):
+        each_dir = (self.directions - 1) / 2
+        each_speed = (self.speeds - 1) / 2
+        self.dirspace = np.linspace(-self.dhead * each_dir,
+                                    self.dhead * each_dir,
+                                    self.directions)
+        self.speedspace = np.linspace(-self.dspeed * each_speed,
+                                      self.dspeed * each_speed,
+                                      self.speeds)
 
-class DiscreteFlat4(DiscreteAction):
-    def __init__(self):
-        super(DiscreteFlat4, self).__init__()
+    def map(self, action, state: WebotState):
+        if self.mode == "flatten":
+            if not isinstance(action, int):
+                raise TypeError("Action must be int.")
+            dir_idx = action % len(self.dirspace)
+            speed_idx = int((action - dir_idx) / len(self.speedspace))
+        elif self.mode == "tuple":
+            dir_idx = action[0]
+            speed_idx = action[1]
 
-    def _init(self, env):
-        env.act_tpl = (4, 1)
-        super()._init(env)
+        # get action difference and add to base action = latest state info
+        action_dx = (self.dirspace[dir_idx], self.speedspace[speed_idx])
+        action = utils.add_tuples(state.pre_action, action_dx)
+        return WebotAction(action)
 
 
 class ContinuousAction(Action):
@@ -44,9 +91,8 @@ class ContinuousAction(Action):
         super(ContinuousAction, self).__init__()
         self.action_space = Box(-1, 1, shape=(2,), dtype=np.float32)
 
-    # if type is fake then bla
-    # def action_map(self, action):
-    #     orientation, length = action
-    #     orientation = utils.id_in_range(-1, 1, self.num_of_directions, orientation)
-    #     length = utils.id_in_range(-1, 1, self.steps, length) + self.step_range[0]
-    #     return orientation, length
+    def map(self, action_dx, state: WebotState):
+        # TODO: relative, absolute
+        action_dx = tuple(action_dx)
+        action = utils.add_tuples(state.pre_action, action_dx)
+        return WebotAction(action)
