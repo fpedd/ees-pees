67fb731cf4dcce6601c72581be605c1b1c8e3844
Per Joachims <p.joachims@gmail.com>
2020-05-27 19:07:31 +0200

    Capital Letter Config

---
+++ backend/Config.py
@@ -0,0 +1,10 @@
+class WebotConfig(object):
+
+    def __init__(self):
+        self.IP = "127.0.0.1"
+        self.CONTROL_PORT = 6969
+        self.BACKEND_PORT = 6970
+        self.PACKET_SIZE = 1496
+        self.DIST_VECS = 360
+        self.TIME_OFFSET_ALLOWED = 1.0
+        self.length = 100
