b183468f0d4f07e5069e7faa6898dffe3d59ff56
Per Joachims <p.joachims@gmail.com>
2020-05-27 20:41:50 +0200

    adapted james to change in environment

--- backend/james.py
+++ backend/james.py
@@ -3,6 +3,8 @@ from pynput import keyboard
 
 import communicate
 import environment
+from Action import ContinuousAction
+from webot import WebotAction
 
 
 class Agent(abc.ABC):
@@ -29,13 +31,14 @@ class RndWebotAgent(Agent):
 
 class WebotCtrAgent(Agent):
     def __init__(self):
-        self.dheading = 3
-        self.dspeed = 3
-        self.env = environment.WebotsEnv()
+        self.dheading = 0.05
+        self.dspeed = 0.05
+        self.env = environment.WebotsEnv(action_class=ContinuousAction)
+        self.env.recv()
         self._init_action()
 
     def _init_action(self):
-        self.act = communicate.WebotAction()
+        self.act = WebotAction()
         self.act.speed = 0
         self.act.heading = 0
 
@@ -54,7 +57,7 @@ class WebotCtrAgent(Agent):
             self.act.heading += self.dheading
         else:
             return
-        self.env.step(self.act)
+        self.env.step((self.act.heading, self.act.speed))
 
     def on_release(self, key):
         if key == keyboard.Key.esc:
