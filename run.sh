9f24acf4e040e01680f89cf38fe41a4c4b6fea27
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-05-29 16:21:47 +0200

    internal_ctrl to internal

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ export WEBOTS_HOME=/usr/local/webots
 
 ### Path to Webots World and Internal Controller
 WORLD_FILE=/webots/worlds/prototypes.wbt
-INT_CTRL=/webots/controllers/internal_ctrl
+INT_CTRL=/webots/controllers/internal
 
 ### Path to Backend Script
 BCKND_SCRIPT=/backend/james.py
