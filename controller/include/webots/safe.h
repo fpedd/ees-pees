5ea1fb4b2dad1abd5db1f3dbefc5bc76afa716bf
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-05-30 02:18:40 +0200

    add safety placeholder functions

--- controller/include/webots/safe.h
+++ controller/include/webots/safe.h
@@ -4,4 +4,8 @@
 // This will contain all code to check if the robot collides with some object.
 // This will then also provide funtions that help to keep the robot safe.
 
+#include "backend/backend_com.h"
+
+int safety_check(bcknd_to_ext_msg_t *bcknd_to_ext);
+
 #endif // SAFE_H
--- controller/src/webots/safe.c
+++ controller/src/webots/safe.c
@@ -1,1 +1,16 @@
 #include "webots/safe.h"
+
+
+int safety_check(bcknd_to_ext_msg_t *bcknd_to_ext) {
+
+    (void) bcknd_to_ext;
+
+    // TODO: here we need to check if we are hitting smth and if
+    // so we need to set the speed (and maybe heading) to 0  or smth similar
+
+    // if hitting then
+    // bcknd_to_ext->speed = 0;
+    // bcknd_to_ext->heading = 0;
+
+    return 0;
+}
