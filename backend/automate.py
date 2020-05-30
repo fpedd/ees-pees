b41f9237e4468290e3d39250ce55f31d7e1968a3
Per Joachims <p.joachims@gmail.com>
2020-05-30 16:17:50 +0200

    minor changes in automate

--- backend/automate.py
+++ backend/automate.py
@@ -6,12 +6,13 @@ import numpy as np
 from enum import IntEnum
 import psutil
 
-import config
+from Config import WebotConfig
 
 # class WebotConfig():
 #     """NO need fuer aktion, fuell ich selber."""
 #     def __init__(self):
 #         pass
+
 
 class FunctionCode(IntEnum):
     UNDEFINED = -1
@@ -26,30 +27,22 @@ class ReturnCode(IntEnum):
     SUCCESS = 0
     ERROR = 1
 
+
 class WebotCtrl():
-    def __init__(self):
+    def __init__(self, config: WebotConfig = WebotConfig()):
+        self.config = config
         self.sock = None
         self.client_sock = None
         self.address = None
         self.return_code = ReturnCode.SUCCESS
-        self.lidar_min_range = -1.0
-        self.lidar_max_range = -1.0
-        self.sim_time_step = 0
-
-        # config of webots internal controller (normal robot operations)
-        self.w_config = config.WebotConfig()
-        # config of webots supervisor controller (automated operations)
-        self.s_config = config.SupervisorConfig()
 
     def init(self):
-        # nothing to do here
         self.compile_program()
         self.start_program()
         # time.sleep(5.0)
         self.establish_connection()
 
     def close(self):
-        # nothing to do here
         self.close_connection()
         self.close_program()
 
@@ -85,11 +78,11 @@ class WebotCtrl():
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # set buffer size to packet size to store only latest package
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF,
-                             self.s_config.PACKET_SIZE)
-        # return self.sock.connect((self.s_config.IP, self.s_config.PORT))
-        self.sock.bind((self.s_config.IP, self.s_config.PORT))
+                             self.config.PACKET_SIZE_S)
+        # return self.sock.connect((self.config.IP, self.config.PORT))
+        self.sock.bind((self.config.IP_S, self.config.PORT_S))
         self.sock.listen(5)
-        print("Accepting on Port: ", self.s_config.PORT)
+        print("Accepting on Port: ", self.config.PORT_S)
         (self.client_sock, self.address) = self.sock.accept()
         # print("Accepting done")
         # data = struct.pack('iiiii', 0, 1, 2, 3, 4)
@@ -99,27 +92,34 @@ class WebotCtrl():
         # close tcp connection to webot supervisor
         self.sock.close()
 
-    def start_env(self, seed=1, fast_simulation=False, num_obstacles=10, world_size=10, target_x=0.5, target_y=0.5):
+    def start_env(self, seed=1, waiting_time=1):
         data = struct.pack('5i2f',
                            FunctionCode.START,
                            seed,
-                           int(fast_simulation),
-                           num_obstacles,
-                           world_size,
-                           target_x,
-                           target_y)
+                           int(self.config.fast_simulation),
+                           self.config.num_obstacles,
+                           self.config.world_size,
+                           self.config.target_x,
+                           self.config.target_y)
         self.client_sock.send(data)
+        time.sleep(waiting_time)
+        self.get_metadata()
 
     def get_metadata(self):
-        buffer = self.client_sock.recv(self.s_config.PACKET_SIZE)
+        buffer = self.client_sock.recv(self.config.PACKET_SIZE_S)
         self.return_code = struct.unpack('i', buffer[0:4])[0]
-        self.lidar_min_range = struct.unpack('f', buffer[4:8])[0]
-        self.lidar_max_range = struct.unpack('f', buffer[8:12])[0]
-        self.sim_time_step = struct.unpack('i', buffer[12:16])[0]
+        self.config.lidar_min_range = struct.unpack('f', buffer[4:8])[0]
+        self.config.lidar_max_range = struct.unpack('f', buffer[8:12])[0]
+        self.config.sim_time_step = struct.unpack('i', buffer[12:16])[0]
 
     def reset_environment(self):
         # environment sollte sein wie beim start der simulation
         data = struct.pack('iiiii', FunctionCode.RESET, 0, 0, 0, 0)
+        self.client_sock.send(data)
+
+    def close_environment(self):
+        # environment sollte sein wie beim start der simulation
+        data = struct.pack('iiiii', FunctionCode.CLOSE, 0, 0, 0, 0)
         self.client_sock.send(data)
 
     def print(self):
@@ -132,6 +132,10 @@ class WebotCtrl():
 
 
 class ExtCtrl():
+    def init(self):
+        self.compile()
+        self.start()
+
     def compile(self):
         self.close()
         subprocess.call(["make", "clean"], cwd="../controller")
