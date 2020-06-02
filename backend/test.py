3df10b18332a35280cd088ad6d0db82c65a68ce9
ShanY-33 <51049980+ShanY-33@users.noreply.github.com>
2020-06-02 22:06:20 +0200

    pytest&pylint

--- backend/test.py
+++ backend/test.py
@@ -1,10 +1,14 @@
 import pytest
 
-class TestClass:
-    def test_one(self):
+class test_class1:
+    def test_pass01(self):
         x = "this"
         assert "h" in x
 
-    def test_two(self):
-        x = "hello"
-        assert hasattr(x, "check")
+    # def test_fail(self):
+    #     x = "hello"
+    #     assert hasattr(x, "check")
+
+class test_class2:
+    def test_pass02(self):
+        assert 1+1 == 2
