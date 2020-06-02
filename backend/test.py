7772b7de558041216d91635589e56ef514082aa4
ShanY-33 <51049980+ShanY-33@users.noreply.github.com>
2020-06-02 22:25:29 +0200

    pytest

--- backend/test.py
+++ backend/test.py
@@ -1,10 +1,10 @@
 import pytest
 
-class test_class1:
-    def test_pass01(self):
-        x = "this"
-        assert "h" in x
 
-    def test_fail(self):
-        x = "hello"
-        assert hasattr(x, "check")
+def test_pass01(self):
+    x = "this"
+    assert "h" in x
+
+def test_fail(self):
+    x = "hello"
+    assert hasattr(x, "check")
