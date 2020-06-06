0de79b052aa61defd7c7652e6c66aca94b496b30
Per Joachims <p.joachims@gmail.com>
2020-06-06 17:00:06 +0200

    return or set seed

--- backend/utils.py
+++ backend/utils.py
@@ -3,12 +3,14 @@ import pickle
 import time
 
 
-def set_random_seed():
+def np_random_seed(set=True):
     """Use current time to set seed to something random."""
     t = 1000 * time.time()
     seed = int(t) % 2**32
-    np.random.seed(seed)
-    return seed
+    if set is True:
+        np.random.seed(seed)
+    else:
+        return seed
 
 
 def seed_list(seed, n=100, low=0, high=10**6):
