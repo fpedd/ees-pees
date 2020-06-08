0d511dc6951192a5794c96190ed73f77e4d2e14e
Jonas <jonas.dommes@gmail.com>
2020-06-08 13:30:38 +0200

    make supervisor compilable

--- webots/controllers/supervisor/include/sv_com.h
+++ webots/controllers/supervisor/include/sv_com.h
@@ -2,16 +2,16 @@
 #define SV_COM_H
 
 enum function_code {
-	UNDEFINED = -1
-    NO_FUNCTION = 0
-    START = 1
-    RESET = 2
+	FUNC_UNDEF = -1,
+    NO_FUNCTION = 0,
+    START = 1,
+    RESET = 2,
     CLOSE = 3
 };
 
 enum return_code {
-    UNDEFINED = -1
-    SUCCESS = 0
+    RET_UNDEF = -1,
+    SUCCESS = 0,
     ERROR = 1
 };
 
@@ -19,7 +19,7 @@ enum return_code {
 typedef struct {
 	enum return_code return_code;   // return_code [enum return_code]
 	int sim_time_step;              // simulation time_step in ms [int]
-	double[2] target;               // target position [double[2]]
+	double target[2];               // target position [double[2]]
 } __attribute__((packed)) sv_to_bcknd_msg_t;
 
 // supervisor <-- backend
@@ -37,6 +37,8 @@ int sv_connect();
 int sv_send(sv_to_bcknd_msg_t data);
 
 int sv_recv(bcknd_to_sv_msg_t *data);
+
+int sv_close();
 
 
 #endif //SV_COM_H
--- webots/controllers/supervisor/supervisor.c
+++ webots/controllers/supervisor/supervisor.c
@@ -21,8 +21,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-//#define COM_EN
-//#include "sv_com.h"
+// #define COM_EN
+#include "sv_com.h"
 #include "sv_functions.h"
 
 #define STANDARD_SCALE 0.25
@@ -32,10 +32,10 @@ int main() {
 
 	wb_robot_init();
 	int timestep = wb_robot_get_basic_time_step();
-	
+
 /*	// **************** COMMUNICATION TEST ***************************
 	// *********** comment out if testing without backend*******
-	
+
 	// recv test message
 	bcknd_to_sv_msg_t test_buf;
 
@@ -63,62 +63,62 @@ int main() {
 	// **************** COMMUNICATION TEST END **********************
 */
 
-	bcknd_to_sv_msg_t recv_buffer = {.function_code = UNDEFINED};
-	sv_to_bcknd_msg_t send_buffer = {.return_code = UNDEFINED};
-	
+	bcknd_to_sv_msg_t recv_buffer = {.function_code = FUNC_UNDEF};
+	sv_to_bcknd_msg_t send_buffer = {.return_code = RET_UNDEF};
+
 	sv_world_def *world = sv_simulation_init();
-	
+
 	 // establish coms to backend
 	int ret_connect = sv_connect();
 	if(ret_connect) {
 		fprintf(stderr, "SUPERVISOR: Can't connect to backend\n");
 	}
-	
+
 	while(recv_buffer.function_code != START) {
 		sv_recv(&recv_buffer);
 	}
-	
+
 	sv_world_init(world, recv_buffer.world_size, STANDARD_SCALE, recv_buffer.num_obstacles, recv_buffer.fast_simulation);
 	sv_world_generate(world, recv_buffer.seed);
-	
+
 	send_buffer.return_code = SUCCESS;
 	send_buffer.sim_time_step = timestep;
 	send_buffer.target[0] = world->target[0];
 	send_buffer.target[1] = world->target[1];
-	
-	sv_send(&send_buffer);
+
+	sv_send(send_buffer);
 
 	sv_simulation_start(world);
-	
+
 	while (wb_robot_step(0) != -1) {
-		
+
 		sv_recv(&recv_buffer);
-		
+
 		if(recv_buffer.function_code == START) {
 			sv_simulation_stop();
 			sv_world_clear(world);
 			sv_world_init(world, recv_buffer.world_size, STANDARD_SCALE, recv_buffer.num_obstacles, recv_buffer.fast_simulation);
-			sv_world_generate(world, recv_buffer.seed);	
-			
+			sv_world_generate(world, recv_buffer.seed);
+
 			send_buffer.return_code = SUCCESS;
 			send_buffer.sim_time_step = timestep;
 			send_buffer.target[0] = world->target[0];
 			send_buffer.target[1] = world->target[1];
-			
-			sv_send(&send_buffer);
-			
+
+			sv_send(send_buffer);
+
 			sv_simulation_start(world);
 		} else if(recv_buffer.function_code == RESET) {
 			sv_simulation_stop();
-			sv_world_generate(world, recv_buffer.seed);	
-			
+			sv_world_generate(world, recv_buffer.seed);
+
 			send_buffer.return_code = SUCCESS;
 			send_buffer.sim_time_step = timestep;
 			send_buffer.target[0] = world->target[0];
 			send_buffer.target[1] = world->target[1];
-			
-			sv_send(&send_buffer);
-			
+
+			sv_send(send_buffer);
+
 			sv_simulation_start(world);
 		} else if(recv_buffer.function_code == CLOSE) {
 			sv_simulation_stop();
@@ -126,11 +126,11 @@ int main() {
 			break; //quit should also break the while loop
 		}
 	};
-	
-	tcp_close();
-	
+
+	sv_close();
+
 	sv_simulation_cleanup(world);
-	
+
 	wb_robot_cleanup();
 
 	return EXIT_SUCCESS;
