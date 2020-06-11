a2f7fa2302cf1f1d814ab04df8e813b283174ecb
Per Joachims <p.joachims@gmail.com>
2020-06-11 20:09:27 +0200

    add tanh distance penalty

--- backend/webotsgym/evaluate.py
+++ backend/webotsgym/evaluate.py
@@ -1,4 +1,5 @@
 import numpy as np
+import abc
 
 from webotsgym.config import WebotConfig
 
@@ -9,25 +10,13 @@ class Evaluate(object):
         self.config = config
         self.reward_range = (-100, 100)
 
+    @abc.abstractmethod
     def calc_reward(self):
-        """Calculate reward."""
-        # calculate base value for reward
-        N = self.env.config.world_size
-        base_v = np.sqrt(2) * N
+        pass
 
-        # get distance and crash penalties
-        distance_penalty = self.env.get_target_distance()
-        crash = self.env.state.touching
-
-        val = base_v * (1 - crash) - distance_penalty
-        return val * self.reward_range[1] / base_v
-
+    @abc.abstractmethod
     def check_done(self):
-        if self.env.iterations % self.config.reset_env_after == 0:
-            return True
-        if self.env.get_target_distance() < 0.1:
-            return True
-        return False
+        pass
 
 
 class EvaluateMats(Evaluate):
@@ -59,40 +48,29 @@ class EvaluateMats(Evaluate):
                 reward = reward - 10
         return reward
 
+    def check_done(self):
+        if self.env.iterations % self.config.reset_env_after == 0:
+            return True
+        if self.env.get_target_distance() < 0.1:
+            return True
+        return False
+
 
 class EvaluatePJ0(Evaluate):
     def __init__(self, env, config: WebotConfig = WebotConfig()):
         super(EvaluatePJ0, self).__init__(env, config)
-        self.reward_range = (-1000, 1000)
+        self.reward_range = (-100, 100)
 
     def calc_reward(self):
-        """Calculate reward function.
-
-        Idea(Mats):
-        - negative reward for normal move so that james moves faster to goal
-        - still lower negative reward if james gets closer to goal
-        - high positive award for reaching it
-        - high negative award to hitting a wall
-        - epsilon only to divide never by 0
-
-        """
-        if self.env.state.touching is True:
-            return self.reward_range[0]
-        elif self.env.get_target_distance() < 0.1:
+        if self.env.get_target_distance() < 0.1:
             return self.reward_range[1]
         else:
-            epsilon = 10**-5
-            cost_step = 1
-            distance = self.env.get_target_distance() + epsilon
-            cost_distance = (distance**0.4) / (distance)
-            reward_factor = -1
-            reward = reward_factor * (cost_step * cost_distance)
-        return reward
+            dist = self.env.get_target_distance()
+            denom = self.env.max_distance / 2
+            return -1 * np.tanh(dist / denom)
 
     def check_done(self):
         if self.env.iterations % self.config.reset_env_after == 0:
-            return True
-        if self.env.state.touching is True:
             return True
         if self.env.get_target_distance() < 0.1:
             return True
