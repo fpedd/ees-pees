d67e01ba9b25633041f94b1cf9dcc2c3ff187e22
Per Joachims <p.joachims@gmail.com>
2020-06-15 09:57:40 +0200

    adding step penalty as functions to be modulized in classes

--- backend/webotsgym/evaluate.py
+++ backend/webotsgym/evaluate.py
@@ -4,11 +4,21 @@ import abc
 from webotsgym.config import WebotConfig
 
 
+def step_penalty_04(env, step_base=-1, eps=10**-5):
+    distance = env.get_target_distance() + eps
+    dist_fac = (distance**0.4) / (distance)
+    return step_base * dist_fac
+
+
+def step_penalty_tanh(env, step_base=-1):
+    distance = env.get_target_distance()
+    dist_fac = np.tanh(distance / (0.5 * env.max_distance))
+    return step_base * dist_fac
+
+
 class Evaluate(object):
     def __init__(self, env, config: WebotConfig = WebotConfig()):
         self.env = env
-        self.config = config
-        self.reward_range = (-100, 100)
 
     @abc.abstractmethod
     def calc_reward(self):
