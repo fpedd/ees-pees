685f21a958234559cc5b60dbb88acefdb0fefa3f
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-06-16 01:36:35 +0200

    comment fix in safe

--- controller/src/webots/safe.c
+++ controller/src/webots/safe.c
@@ -12,7 +12,7 @@ int safety_check(bcknd_to_ext_msg_t *bcknd_to_ext) {
 	// TODO: here we need to check if we are hitting smth and if
 	// so we need to set the speed (and maybe heading) to 0  or smth similar
 
-	// if hitting then
+	// maybe: if hitting then
 	// bcknd_to_ext->speed = 0;
 	// bcknd_to_ext->heading = 0;
 
