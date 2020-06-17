c22179ef26039cfc8baf2a0d679d93342880e3a6
Wladimir <a@b.c>
2020-06-17 16:21:48 +0200

    hotfix enum

--- webots/controllers/supervisor/supervisor.c
+++ webots/controllers/supervisor/supervisor.c
@@ -31,7 +31,7 @@ void print_recvd_packet(bcknd_to_sv_msg_t *packet) {
 	printf("=========== received packet ===========\n");
 	printf("function_code: %d\n", packet->function_code);
 	printf("seed: %d\n", packet->seed);
-	printf("fast_simulation: %d\n", packet->fast_simulation);
+	printf("mode: %d\n", packet->mode);
 	printf("num_obstacles: %d\n", packet->num_obstacles);
 	printf("world_size: %d\n", packet->world_size);
 	printf("scale: %f\n", packet->scale);
