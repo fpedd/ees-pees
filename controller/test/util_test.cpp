1be7afe1bf714006807608d0086417a5321f58d3
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-06-18 00:12:53 +0200

    fix unit test in util

--- controller/test/util_test.cpp
+++ controller/test/util_test.cpp
@@ -16,19 +16,19 @@ TEST(util, heading_in_norm) {
 
 TEST(util, round_with_factor) {
 
-	ASSERT_NEAR(round_with_factor(-0.23481, 0.3), -0.3, 1.0e-5);
-	ASSERT_NEAR(round_with_factor(-0.89712, 0.3), -0.9, 1.0e-5);
-	ASSERT_NEAR(round_with_factor(-1.42131, 0.3), -1.5, 1.0e-5);
-	ASSERT_NEAR(round_with_factor(-0.74523, 0.3), -0.6, 1.0e-5);
+	ASSERT_NEAR(round_with_factor(-0.23481, 0.3), -0.15, 1.0e-5);
+	ASSERT_NEAR(round_with_factor(-0.89712, 0.3), -0.75, 1.0e-5);
+	ASSERT_NEAR(round_with_factor(-1.42131, 0.3), -1.35, 1.0e-5);
+	ASSERT_NEAR(round_with_factor(-0.74523, 0.3), -0.75, 1.0e-5);
 
-	ASSERT_NEAR(round_with_factor(0.23481, 0.25), 0.25, 1.0e-5);
-	ASSERT_NEAR(round_with_factor(0.89712, 0.25), 1.0, 1.0e-5);
-	ASSERT_NEAR(round_with_factor(1.42131, 0.25), 1.5, 1.0e-5);
-	ASSERT_NEAR(round_with_factor(0.74523, 0.25), 0.75, 1.0e-5);
+	ASSERT_NEAR(round_with_factor(0.23481, 0.25), 0.125, 1.0e-5);
+	ASSERT_NEAR(round_with_factor(0.89712, 0.25), 0.875, 1.0e-5);
+	ASSERT_NEAR(round_with_factor(1.42131, 0.25), 1.375, 1.0e-5);
+	ASSERT_NEAR(round_with_factor(0.74523, 0.25), 0.625, 1.0e-5);
 
-	ASSERT_NEAR(round_with_factor(0.23481, 0.5), 0.0, 1.0e-5);
-	ASSERT_NEAR(round_with_factor(0.89712, 0.5), 1.0, 1.0e-5);
-	ASSERT_NEAR(round_with_factor(1.42131, 0.5), 1.5, 1.0e-5);
-	ASSERT_NEAR(round_with_factor(0.74523, 0.5), 0.5, 1.0e-5);
+	ASSERT_NEAR(round_with_factor(0.23481, 0.5), 0.25, 1.0e-5);
+	ASSERT_NEAR(round_with_factor(0.89712, 0.5), 0.75, 1.0e-5);
+	ASSERT_NEAR(round_with_factor(1.42131, 0.5), 1.25, 1.0e-5);
+	ASSERT_NEAR(round_with_factor(0.74523, 0.5), 0.75, 1.0e-5);
 
 }
