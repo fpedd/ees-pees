17a9c2cde111e0777f4b071485fbea2c67e63fde
Per Joachims <p.joachims@gmail.com>
2020-06-18 09:36:03 +0200

    schnitty PEP8

--- backend/schnitty.py
+++ backend/schnitty.py
@@ -20,7 +20,8 @@ class Schnitty():
         self.act.heading = 0
 
     def action(self):
-        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
+        with keyboard.Listener(on_press=self.on_press,
+                               on_release=self.on_release) as listener:
             listener.join()
 
     def on_press(self, key):
