8c197cf2ebf28b47a92dde85138d33aac9db5475
Per Joachims <p.joachims@gmail.com>
2020-06-18 13:02:00 +0200

    lidar mapping

--- backend/webotsgym/observation.py
+++ backend/webotsgym/observation.py
@@ -40,13 +40,29 @@ class GridObservation(Observation):
 
     @property
     def observation_space(self):
-        return spaces.Box(-np.inf, np.inf, shape=(4,), dtype=np.float32)
+        return spaces.Box(-np.inf, np.inf, shape=(8,), dtype=np.float32)
+
+    @property
+    def lidar(self):
+        dists = self.env.state.get_grid_distances(4)
+        dists = np.flip(dists)
+        roll = int(len(dists) / 4)
+        dists = np.roll(dists, roll)
+        dists = (dists - 0.2) / 0.5
+        return np.round(dists)
+
+    @property
+    def gps_actual(self):
+        return np.round(0.5 + np.array(self.env.state.gps_actual) * 2)
+
+    @property
+    def gps_target(self):
+        return np.round(0.5 + np.array(self.env.gps_target) * 2)
 
     def get(self):
         arr = np.empty(0)
-        arr = np.hstack((arr, np.array(self.env.state.gps_actual) * 2))
-        arr = np.hstack((arr, np.array(self.env.gps_target) * 2))
-        arr = np.round(0.5 + arr)
-        # arr = np.hstack((arr,np.array(self.env.state.get_grid_distances(4))))
+        arr = np.hstack((arr, self.gps_actual))
+        arr = np.hstack((arr, self.gps_target))
+        arr = np.hstack((arr, self.lidar))
         # arr = np.hstack((arr, np.array(self.env.state.action_denied)))
         return arr
