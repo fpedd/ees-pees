cdc636b565e2ccd0e124a2e21d0a3de9e8a93f48
Per Joachims <p.joachims@gmail.com>
2020-06-18 17:09:33 +0200

    renaming in automation_test, timmy gets stuck

--- backend/automation_test.py
+++ backend/automation_test.py
@@ -1,22 +1,18 @@
 import time
-import subprocess
 
-from james import RndWebotAgent
+from james import James
+from timmy import Timmy
 import webotsgym.automate as automate
 
 print("start webot")
 w = automate.WebotCtrl()
 w.init()
 
-# print("start external")
-# e = automate.ExtCtrl()
-# e.init()
-
 print("start environment")
 w.start_env()
 
 print("random actions")
-timmy = RndWebotAgent()
+timmy = Timmy()
 for _ in range(20):
     timmy.action()
 
@@ -26,7 +22,7 @@ time.sleep(3.0)
 w.reset_environment()
 
 print("random actions")
-timmy = RndWebotAgent()
+timmy = James()
 for _ in range(20):
     timmy.action()
 
