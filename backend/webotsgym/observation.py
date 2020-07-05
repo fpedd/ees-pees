eea604110a6d5b79d2d4897c27a9a3bb608ec55a
mwunderbar <kipper.mats@gmail.com>
2020-07-05 22:13:18 +0200

    Added new Observation for continuous space

--- backend/webotsgym/observation.py
+++ backend/webotsgym/observation.py
@@ -33,6 +33,49 @@ class Observation():
         arr = np.hstack((arr, np.array(self.env.state.lidar_absolute)))
         return arr
 
+class StepObservation(Observation):
+    def __init__(self, env):
+        super(StepObservation, self).__init__(env)
+
+    @property
+    def observation_space(self):
+        return spaces.Box(-np.inf, np.inf, shape=(20,),
+                          dtype=np.float32)
+
+    @property
+    def lidar(self):
+        """Build new lidar function with average distance."""
+        dists = self.env.state.get_grid_distances(4)
+        dists = np.flip(dists)
+        roll = int(len(dists) / 4)
+        dists = np.roll(dists, roll)
+        dists = (dists - 0.2) / 0.5
+        return np.round(dists)
+
+    def get(self):
+        """Get standard observation.
+
+        sim_time:   1
+        gps_actual: 2
+        gps_target: 2
+        heading:    1
+        steering:   1
+        touching:   1
+        lidar:     12
+        -------------
+        total:     20
+        """
+        arr = np.empty(0)
+        arr = np.hstack((arr, np.array(self.env.state.sim_time)))
+        arr = np.hstack((arr, np.array(self.env.state.gps_actual)))
+        arr = np.hstack((arr, np.array(self.env.gps_target)))
+        arr = np.hstack((arr, np.array(self.env.state.heading)))
+        arr = np.hstack((arr, np.array(self.env.state.steering)))
+        arr = np.hstack((arr, np.array(self.env.state.touching)))
+        arr = np.hstack((arr, np.array(self.lidar)))
+        return arr
+
+
 
 class GridObservation(Observation):
     def __init__(self, env):
