e84f35cacab61061b5e21cbc363c659f9959a922
Per Joachims <p.joachims@gmail.com>
2020-07-10 09:18:58 +0200

    Proper way to kill blocking python process

--- backend/webotsgym/com/__init__.py
+++ backend/webotsgym/com/__init__.py
@@ -1,9 +1,9 @@
 from webotsgym.com.package import PacketIn, PacketOut, ActionOut, PacketType
 from webotsgym.com.state import WbtState
-from webotsgym.com.automate import WbtCtrl
+from webotsgym.com.automate import WbtCtrl, kill_spv_connection
 from webotsgym.com.communicate import Communication
 
 __all__ = ['PacketIn', 'PacketOut', 'ActionOut', 'PacketType',
            'WbtState',
-           'WbtCtrl',
+           'WbtCtrl', 'kill_spv_connection',
            'Communication']
