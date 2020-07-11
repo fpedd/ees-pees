30a93e25a7c18544c6442f487b8eba7ebf3a05dd
Per Joachims <p.joachims@gmail.com>
2020-07-11 10:07:15 +0200

    Dummy config grid action

--- backend/webotsgym/env/grid/action.py
+++ backend/webotsgym/env/grid/action.py
@@ -10,7 +10,7 @@ class WbtActGrid(WbtAct):
     2: Left  -> Down  (3)
     3: Up    -> Right (4)
     """
-    def __init__(self,env):
+    def __init__(self, config=None):
         self.action_space = Discrete(4)
         self.direction_type = "steering"  # just a dummy
         self.type = "grid"
