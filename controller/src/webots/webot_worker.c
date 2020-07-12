c25ff64f58a2723e5946242166f233e65173ed6a
Jonas <jonas.dommes@gmail.com>
2020-07-12 10:49:44 +0200

    fix actual speed scaling

--- controller/src/webots/webot_worker.c
+++ controller/src/webots/webot_worker.c
@@ -111,7 +111,7 @@ int webot_format_wb_to_bcknd(data_to_bcknd_msg_t* data_to_bcknd,
 
 	// cast sim time and robot speed to float
 	data_to_bcknd->sim_time = (float) data_from_wb.sim_time;
-	data_to_bcknd->speed = (float) speed_with_dir(data_from_wb) / init_data.maxspeed;
+	data_to_bcknd->speed = (float) speed_with_dir(data_from_wb) / 0.03;
 
 	// calculate projecions for 3D gps/compass data_to_bcknd to bcknd format
 	// (x, z coorinates represent horizontal plane in webots system)
