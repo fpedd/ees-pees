3a7182a1e72847f9a8a7daf7ca149330dbc3a562
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-07-13 17:33:10 +0200

    test smth

--- backend/webotsgym/com/automate/util.py
+++ backend/webotsgym/com/automate/util.py
@@ -5,8 +5,8 @@ def get_repo_dir():
     """Get directory of ees-pees."""
     p = os.path.abspath('..')
     print("P: ", p)
-    home_dir = p.split("/ees-pees")[0]
+    home_dir = p.split("/ees-pees/backend")[0]
     print("HOME_DIR: ", home_dir)
-    repo_dir = os.path.join(home_dir, "ees-pees")
+    repo_dir = os.path.join(home_dir, "ees-pees/backend")
     print("REPO_DIR: ", repo_dir)
     return repo_dir
