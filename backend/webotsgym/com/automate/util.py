1b0caead3673e5225342409f8f644d97728d0e24
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-07-13 17:28:03 +0200

    make pylint happy

--- backend/webotsgym/com/automate/util.py
+++ backend/webotsgym/com/automate/util.py
@@ -6,7 +6,9 @@ def get_repo_dir():
     p = os.path.abspath('..')
     print("P: ", p)
     home_dir = p.split("/ees-pees")[0]
-	print("HOME_DIR: ", home_dir)
+    print("HOME_DIR: ", home_dir)
     repo_dir = os.path.join(home_dir, "ees-pees")
-	print("REPO_DIR: ", repo_dir)
+    print("REPO_DIR: ", repo_dir)
     return repo_dir
+
+ees-pees
