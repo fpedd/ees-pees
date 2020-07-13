0a62afe8bfec92c6828f79f9c5e173847ddf1064
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-07-13 17:27:05 +0200

    add some prints to get_repo_dir()

--- backend/webotsgym/com/automate/util.py
+++ backend/webotsgym/com/automate/util.py
@@ -4,6 +4,9 @@ import os
 def get_repo_dir():
     """Get directory of ees-pees."""
     p = os.path.abspath('..')
+    print("P: ", p)
     home_dir = p.split("/ees-pees")[0]
+	print("HOME_DIR: ", home_dir)
     repo_dir = os.path.join(home_dir, "ees-pees")
+	print("REPO_DIR: ", repo_dir)
     return repo_dir
