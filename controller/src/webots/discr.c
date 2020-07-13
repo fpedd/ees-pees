b41cf04f8d0673598d015613e24edd3e4ef5f2c5
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-07-13 15:59:44 +0200

    refactor discr

--- controller/src/webots/discr.c
+++ controller/src/webots/discr.c
@@ -12,9 +12,15 @@
 #include "webots/navi.h"
 #include "backend/backend_com.h"
 
+// The step size we want to in the webots world [in meter]
 #define STEP_SIZE 0.5
 
+// The current target destination the drive function will drive us to.
+// It will a 2d vector on our "virtual" grid inside the webots world. So its a
+// multiple of STEP_SIZE.
 float target[2];
+
+// The previous target. Needed to revert action in case of action denied.
 float last_target[2];
 
 int discr_init() {
@@ -29,13 +35,15 @@ int discr_step(cmd_to_wb_msg_t *cmd_to_wb, cmd_from_bcknd_msg_t cmd_from_bcknd,
                data_to_bcknd_msg_t data_to_bcknd, init_to_ext_msg_t init_data,
                int start, int action_denied) {
 
-	// lets start where the robot currently is at
+	// Start where the robot currently is at
 	if (start == 1) {
+		// Use rounding to align the target to "nearest" point on "virtual" grid.
 		target[0] = round_with_factor(data_to_bcknd.actual_gps[0], STEP_SIZE);
 		target[1] = round_with_factor(data_to_bcknd.actual_gps[1], STEP_SIZE);
+
+		// Last target and current target are the same for first step
 		last_target[0] = target[0];
 		last_target[1] = target[1];
-		printf("DISCR: start == 1 reached\n");
 	}
 
 	// make sure we only do actions once per message
@@ -46,33 +54,33 @@ int discr_step(cmd_to_wb_msg_t *cmd_to_wb, cmd_from_bcknd_msg_t cmd_from_bcknd,
 		last_target[0] = target[0];
 		last_target[1] = target[1];
 
+		// Do the move coming from backend by adjusting the target accordingly.
 		switch (cmd_from_bcknd.move) {
 			case UP:
-			target[1] += STEP_SIZE;
-			break;
+				target[1] += STEP_SIZE;
+				break;
 			case LEFT:
-			target[0] += STEP_SIZE;
-			break;
+				target[0] += STEP_SIZE;
+				break;
 			case DOWN:
-			target[1] -= STEP_SIZE;
-			break;
+				target[1] -= STEP_SIZE;
+				break;
 			case RIGHT:
-			target[0] -= STEP_SIZE;
-			break;
+				target[0] -= STEP_SIZE;
+				break;
 			case NONE:
 			default:
-			fprintf(stderr, "ERROR: discr step invalid step command\n");
-			break;
+				fprintf(stderr, "ERROR: discr step invalid step command\n");
+				break;
 		}
 
 		last_msg_cnt = cmd_from_bcknd.msg_cnt;
 
 	}
 
-	// print_cood(data_to_bcknd.actual_gps, target);
 	(void) action_denied;
-	// revert to former target
-	// TODO: the action denied is too strict, we need to fix this
+	// The backend actually checks for safety violations. So we have no need for
+	// the action denied flag and reverting.
 	// if (action_denied != 0) {
 	// 	printf("DISCR: Reverted from target [%.2f,%.2f] to [%.2f,%.2f]\n", target[0], target[1], last_target[0], last_target[1]);
 	// 	target[0] = last_target[0];
@@ -82,6 +90,7 @@ int discr_step(cmd_to_wb_msg_t *cmd_to_wb, cmd_from_bcknd_msg_t cmd_from_bcknd,
 	return navigate(cmd_to_wb, data_to_bcknd, init_data, target);
 }
 
+// Function that helps with printing
 void print_cood(float actual[2], float target[2]) {
 	printf("actual: %f %f \n", actual[0], actual[1]);
 	printf("target: %f %f \n", target[0], target[1]);
