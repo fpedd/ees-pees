f164638b15f44bc59bd20199212ad2b538e4a744
ShanY-33 <yinnn33@gmail.com>
2020-07-17 00:09:29 +0200

    update

--- backend/test/unittest_Fakeenv.py
+++ backend/test/unittest_Fakeenv.py
@@ -35,9 +35,9 @@ class TestFakeEnv(unittest.TestCase):
         obs_2 = env.reset()
         pos_actual2 = env.gps_actual
         pos_target2 =  env.gps_target
-        self.assertFalse(pos_actual1 == pos_actual2)
-        self.assertFalse(pos_target1 == pos_target2)
-        self.assertFalse((obs_1 == obs_2).all())
+
+        self.assertFalse(pos_actual1 == pos_actual2 and pos_target1 == pos_target2 and (obs_1 == obs_2).all())
+
     
     def test_step(self):
         """Test step function."""
@@ -46,8 +46,42 @@ class TestFakeEnv(unittest.TestCase):
         action = random.randint(0,3)
         obs, rewards, done, info = env.step(action)
         self.assertFalse((obs_start == obs).all())
-        self.assertTrue(env.reward_range[0] < rewards < env.reward_range[1])
+        self.assertTrue(env.reward_range[0] <= rewards <= env.reward_range[1])
         self.assertIsInstance(done, bool)
+
+    def test_get_target_distance(self):
+        """Test get target distance."""
+        env = self.env
+        actual = env.gps_actual
+        target =  env.gps_target
+        distance = np.sqrt(np.square(actual[0]-target[0]) + np.square(actual[1]-target[1]))
+        self.assertEqual(env.get_target_distance(normalized=False), distance)
+        distance_normalize = distance/ (np.sqrt(2)*env.N)
+        self.assertEqual(env.get_target_distance(normalized=True), distance_normalize)
+
+    def test_gps_visited_count(self):
+        """Test visited count."""
+        env = self.env
+        rx,ry = env.gps_actual
+        vc = env.gps_visited_count
+        env.step(0)
+        if (rx,ry != env.gps_actual):
+            env.step(2)
+            self.assertEqual(env.gps_visited_count, vc+1)
+            env.step(0)
+            env.step(2)
+            self.assertEqual(env.gps_visited_count, vc+2)
+        
+    def test_reset_with_seed(self):
+        """Test reset the env with same seed."""
+        env = self.env
+        obs_1 =  env.reset(seed = 1)
+        pos_actual1 = env.gps_actual
+        pos_target1 =  env.gps_target
+        obs_2 = env.reset(seed = 1)
+        pos_actual2 = env.gps_actual
+        pos_target2 =  env.gps_target
+        self.assertTrue(pos_actual1 == pos_actual2 and pos_target1 == pos_target2 and (obs_1 == obs_2).all())
 
 
 if __name__ == '__main__':
