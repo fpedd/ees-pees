392154b417940b6eec96c307372b241404293b44
Per Joachims <p.joachims@gmail.com>
2020-07-16 19:37:07 +0200

    Proper import Jonas' reward func

--- backend/webotsgym/env/__init__.py
+++ backend/webotsgym/env/__init__.py
@@ -1,8 +1,8 @@
 from webotsgym.env.webotenv import WbtGym
 from webotsgym.env.grid import WbtGymGrid
 from webotsgym.env.action import WbtActContinuous, WbtActDiscrete
-from webotsgym.env.reward import WbtReward, WbtRewardGrid
+from webotsgym.env.reward import WbtReward, WbtRewardGrid, WbtRewardContinuousV1  # noqa E501 
 from webotsgym.env.observation import WbtObs
 
 __all__ = ['WbtGymGrid', 'WbtGym', 'WbtActContinuous', 'WbtActDiscrete',
-           'WbtReward', 'WbtRewardGrid', 'WbtObs']
+           'WbtReward', 'WbtRewardGrid', 'WbtRewardContinuousV1', 'WbtObs']
--- backend/webotsgym/env/reward/steppenalty.py
+++ backend/webotsgym/env/reward/steppenalty.py
@@ -2,17 +2,13 @@ import numpy as np
 from webotsgym.utils import exponential_decay
 
 
-def step_pen_04(env, step_base=-1, eps=10**-5):
-    distance = env.get_target_distance() + eps
-    dist_fac = (distance**0.4) / (distance)
-    return step_base * dist_fac
-
-
 def step_pen_tanh(env, step_base=-1):
+    """Calculate step penalty with tanh."""
     distance = env.get_target_distance()
     dist_fac = np.tanh(distance / (0.5 * env.max_distance))
     return step_base * dist_fac
 
 
 def step_pen_exp(x, step_penalty=-1, lambda_=5):
+    """Calculate step penalty with exponential_decay."""
     return step_penalty * (1 - exponential_decay(x, lambda_=lambda_))
