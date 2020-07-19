147f024271a237ccca96a2c89b2a10d49e2e07a1
mwunderbar <kipper.mats@gmail.com>
2020-07-19 12:29:14 +0200

    DocString grid action.py

--- backend/webotsgym/env/grid/action.py
+++ backend/webotsgym/env/grid/action.py
@@ -10,10 +10,29 @@ class WbtActGrid():
     3: Up    -> Right (4)
     """
     def __init__(self, config=None):
+        """Initialize WbtActGrid class."""
         self.config = config
         self.action_space = Discrete(4)
         self.direction_type = "steering"  # just a dummy
         self.type = "grid"
 
     def map(self, action):
+        """Map fake environment action to webots.
+
+        Description:
+        ------------
+        We use a different mapping in the fake environment as
+        in webots so that we use the function here to map them accordingly
+
+        Parameter:
+        ----------
+        action : integer
+            action of the fake environment
+
+        Return:
+        -------
+        integer
+            action mapped on webots environment
+
+        """
         return int(action + 1)
